ef2a2e0a4bdb71416e2c526c9e6444fa50dd0672
dev-hippo-an <dkstpgud@gmail.com>
2024-01-19 19:11:33 +0900

    feat: request path and chapter mapping

--- adventure_04/internal/handlers/adventure_handler.go
+++ adventure_04/internal/handlers/adventure_handler.go
@@ -6,29 +6,44 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
 )
-
-var templateMap = make(map[string]string)
-
-func init() {
-	templateMap["index"] = "./adventure_04/web/index.gohtml"
-}
 
 type AdventureHandler struct {
 	story *models.Story
 }
 
-func (h AdventureHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	indexHtml, err := os.ReadFile(templateMap["index"])
+var pathHtml string
+
+func init() {
+	templateBytes, err := os.ReadFile("./adventure_04/web/template.gohtml")
 	if err != nil {
 		log.Fatal("error while read index html file; ", err)
 	}
-	tmpl := template.Must(template.New("index").Parse(string(indexHtml)))
-	story := *h.story
-	err = tmpl.Execute(w, story["intro"])
-	if err != nil {
-		log.Fatal("error while execute index template parsing")
+
+	pathHtml = string(templateBytes)
+}
+
+func (h AdventureHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	path := strings.TrimSpace(r.URL.Path)
+	if path == "" || path == "/" {
+		path = "/intro"
 	}
+
+	tmpl := template.Must(template.New(path).Parse(pathHtml))
+	story := *h.story
+
+	if chapter, ok := story[path[1:]]; ok {
+		err := tmpl.Execute(w, chapter)
+		if err != nil {
+			log.Printf("%v\n", err)
+			http.Error(w, "Something went wrong...", http.StatusInternalServerError)
+		}
+		return
+	} else {
+		http.Error(w, "Chapter not found.", http.StatusNotFound)
+	}
+
 }
 
 func NewHandler(s *models.Story) AdventureHandler {
