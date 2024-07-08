496a593a510f5ed30160ae810ce1fa905e31d6d7
hippo-an <dkstpgud@gmail.com>
2024-07-08 19:27:54 +0900

    mombank: grpc server start and listen

---
+++ mombank/gapi/server.go
@@ -0,0 +1,33 @@
+package gapi
+
+import (
+	"fmt"
+
+	db "github.com/hippo-an/tiny-go-challenges/mombank/db/sqlc"
+	"github.com/hippo-an/tiny-go-challenges/mombank/pb"
+	"github.com/hippo-an/tiny-go-challenges/mombank/token"
+	"github.com/hippo-an/tiny-go-challenges/mombank/util"
+)
+
+type Server struct {
+	pb.UnimplementedSimpleBankServer
+	config     util.Config
+	store      db.Store
+	tokenMaker token.Maker
+}
+
+func NewServer(config util.Config, store db.Store) (*Server, error) {
+
+	tokenMaker, err := token.NewPasetoMaker(config.TokenSymmetricKey)
+
+	if err != nil {
+		return nil, fmt.Errorf("cannot create token maker: %w", err)
+	}
+
+	server := &Server{
+		config:     config,
+		store:      store,
+		tokenMaker: tokenMaker,
+	}
+	return server, nil
+}
