bebafe05e6461ea7e82d4ea76bb9df7b254ea245
hippo-an <dkstpgud@gmail.com>
2025-01-17 00:00:04 +0900

    feat: htmx use case hx-swap-oob is only works with id attributes, hx-target, hx-swap, hx-post etc

--- intro_hyper/cmd/main.go
+++ intro_hyper/cmd/main.go
@@ -27,6 +27,70 @@ type Count struct {
 	Count int
 }
 
+type Contact struct {
+	Name  string
+	Email string
+}
+
+func newContact(name, email string) Contact {
+	return Contact{
+		Name:  name,
+		Email: email,
+	}
+}
+
+type Contacts = []Contact
+
+type Data struct {
+	Contacts Contacts
+}
+
+func (d Data) hasEmail(email string) bool {
+	for _, contact := range d.Contacts {
+		if contact.Email == email {
+			return true
+		}
+	}
+	return false
+}
+
+func newData() Data {
+	return Data{
+		Contacts: Contacts{
+			newContact("John", "john@example.com"),
+			newContact("Pale", "pale@example.com"),
+			newContact("Trace", "trace@example.com"),
+			newContact("Jane", "jane@example.com"),
+			newContact("Log", "log@example.com"),
+			newContact("Jump", "jump@example.com"),
+		},
+	}
+}
+
+type FormData struct {
+	Values map[string]string
+	Errors map[string]string
+}
+
+func newFormData() FormData {
+	return FormData{
+		Values: make(map[string]string),
+		Errors: make(map[string]string),
+	}
+}
+
+type Page struct {
+	Data Data
+	Form FormData
+}
+
+func newPage() Page {
+	return Page{
+		Data: newData(),
+		Form: newFormData(),
+	}
+}
+
 func main() {
 	e := echo.New()
 	e.Use(middleware.Logger())
@@ -42,6 +106,35 @@ func main() {
 	e.POST("/count", func(c echo.Context) error {
 		count.Count++
 		return c.Render(http.StatusOK, "count", count)
+	})
+
+	page := newPage()
+
+	e.GET("/contacts", func(c echo.Context) error {
+		return c.Render(http.StatusOK, "contact-index", page)
+	})
+
+	e.POST("/contacts", func(c echo.Context) error {
+		name := c.FormValue("name")
+		email := c.FormValue("email")
+
+		if page.Data.hasEmail(email) {
+			fd := newFormData()
+			fd.Values["name"] = name
+			fd.Values["email"] = email
+			fd.Errors["email"] = "Email already exists"
+			page.Form = fd
+			return c.Render(http.StatusUnprocessableEntity, "form", page.Form)
+		}
+
+		contact := newContact(name, email)
+		page.Data.Contacts = append(
+			page.Data.Contacts,
+			contact,
+		)
+
+		c.Render(http.StatusOK, "form", newFormData())
+		return c.Render(http.StatusOK, "oob-contact", contact)
 	})
 
 	log.Fatal(e.Start(":8080"))
