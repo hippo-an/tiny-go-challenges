446194c8e1da3106c4c13ed825d8af57f5274485
hippo-an <dkstpgud@gmail.com>
2025-11-07 18:31:40 +0900

    types for udp tftp

---
+++ netpro_99/udp_reliable/types.go
@@ -0,0 +1,274 @@
+package udpreliable
+
+import (
+	"bytes"
+	"encoding/binary"
+	"errors"
+	"io"
+	"strings"
+)
+
+const (
+	DataGramSize = 516
+	BlockSize    = DataGramSize - 4
+)
+
+type OpCode uint16 // 첫 2바이트는 Op(Operation) 을 나타내는 코드
+
+const (
+	OpRRQ OpCode = iota + 1
+	_
+	OpData
+	OpAck
+	OpErr
+)
+
+type ErrCode uint16
+
+const (
+	ErrUnknown ErrCode = iota
+	ErrNotFound
+	ErrAccessViolation
+	ErrDiskFull
+	ErrIllegalOp
+	ErrUnknownID
+	ErrFileExists
+	ErrNoUser
+)
+
+type ReadReq struct {
+	FileName string
+	Mode     string
+}
+
+// instance -> bytes
+func (q ReadReq) MarshalBinary() ([]byte, error) {
+	mode := "octet"
+	if q.Mode != "" {
+		mode = q.Mode
+	}
+
+	// OP code + filename + 0 byte + mode + 0 byte
+	cap := 2 + len(q.FileName) + 1 + len(q.Mode) + 1
+
+	b := new(bytes.Buffer)
+	b.Grow(cap)
+
+	err := binary.Write(b, binary.BigEndian, OpRRQ)
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = b.WriteString(q.FileName)
+	if err != nil {
+		return nil, err
+	}
+
+	err = b.WriteByte(0)
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = b.WriteString(mode)
+	if err != nil {
+		return nil, err
+	}
+
+	err = b.WriteByte(0)
+	if err != nil {
+		return nil, err
+	}
+
+	return b.Bytes(), nil
+
+}
+
+// bytes -> instance
+func (q ReadReq) UnmarshalBinary(p []byte) error {
+	r := bytes.NewBuffer(p)
+
+	var code OpCode
+
+	err := binary.Read(r, binary.BigEndian, &code)
+
+	if err != nil {
+		return err
+	}
+
+	if code != OpRRQ {
+		return errors.New("invalid RRQ")
+	}
+
+	q.FileName, err = r.ReadString(0)
+	if err != nil {
+		return errors.New("invalid RRQ")
+	}
+
+	q.FileName = strings.TrimRight(q.FileName, "\x00")
+	if len(q.FileName) == 0 {
+		return errors.New("invalid RRQ")
+	}
+
+	q.Mode, err = r.ReadString(0)
+	if err != nil {
+		return errors.New("invalid RRQ")
+	}
+
+	q.Mode = strings.TrimRight(q.Mode, "\x00")
+	if len(q.Mode) == 0 {
+		return errors.New("invalid RRQ")
+	}
+
+	actual := strings.ToLower(q.Mode)
+	if actual != "octet" {
+		return errors.New("only binary transfers supported")
+	}
+
+	return nil
+}
+
+type Data struct {
+	Block   uint16
+	Payload io.Reader
+}
+
+// instance -> bytes
+func (d *Data) MarshalBinary() ([]byte, error) {
+	b := new(bytes.Buffer)
+	b.Grow(DataGramSize)
+
+	d.Block++
+
+	err := binary.Write(b, binary.BigEndian, OpData)
+	if err != nil {
+		return nil, err
+	}
+
+	err = binary.Write(b, binary.BigEndian, d.Block)
+	if err != nil {
+		return nil, err
+	}
+
+	// BlockSize 만큼만 쓰기
+	_, err = io.CopyN(b, d.Payload, BlockSize)
+	if err != nil {
+		return nil, err
+	}
+
+	return b.Bytes(), nil
+}
+
+func (d *Data) UnmarshalBinary(p []byte) error {
+	if l := len(p); l < 4 || l > DataGramSize {
+		return errors.New("invalid DATA")
+	}
+
+	var opCode OpCode
+	err := binary.Read(bytes.NewReader(p[:2]), binary.BigEndian, &opCode)
+	if err != nil || opCode != OpData {
+		return errors.New("invalid Data")
+	}
+
+	err = binary.Read(bytes.NewReader(p[2:4]), binary.BigEndian, &d.Block)
+	if err != nil {
+		return errors.New("invalid Data")
+	}
+
+	d.Payload = bytes.NewBuffer(p[4:])
+	return nil
+
+}
+
+type Ack uint16
+
+func (a Ack) marshalBinary() ([]byte, error) {
+	cap := 2 + 2
+
+	b := new(bytes.Buffer)
+	b.Grow(cap)
+
+	err := binary.Write(b, binary.BigEndian, OpAck)
+	if err != nil {
+		return nil, err
+	}
+
+	err = binary.Write(b, binary.BigEndian, a)
+	if err != nil {
+		return nil, err
+	}
+
+	return b.Bytes(), nil
+}
+
+func (a *Ack) UnmarshalBinary(p []byte) error {
+	var code OpCode
+
+	r := bytes.NewReader(p)
+	err := binary.Read(r, binary.BigEndian, &code)
+	if err != nil {
+		return err
+	}
+
+	if code != OpAck {
+		return errors.New("invalid ACK")
+	}
+
+	return binary.Read(r, binary.BigEndian, a)
+}
+
+type Err struct {
+	Error   ErrCode
+	Message string
+}
+
+func (e Err) MarshalBinary() ([]byte, error) {
+	cap := 2 + 2 + len(e.Message) + 1
+	b := new(bytes.Buffer)
+	b.Grow(cap)
+
+	err := binary.Write(b, binary.BigEndian, OpErr)
+	if err != nil {
+		return nil, err
+	}
+
+	err = binary.Write(b, binary.BigEndian, e.Error)
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = b.WriteString(e.Message)
+	if err != nil {
+		return nil, err
+	}
+
+	err = b.WriteByte(0)
+	if err != nil {
+		return nil, err
+	}
+
+	return b.Bytes(), nil
+}
+
+func (e *Err) UnmarshalBinary(p []byte) error {
+	r := bytes.NewBuffer(p)
+
+	var code OpCode
+	err := binary.Read(r, binary.BigEndian, &code)
+	if err != nil {
+		return err
+	}
+
+	if code != OpErr {
+		return errors.New("invalid ERROR")
+	}
+
+	err = binary.Read(r, binary.BigEndian, &e.Error)
+	if err != nil {
+		return err
+	}
+
+	e.Message, err = r.ReadString(0)
+	e.Message = strings.TrimRight(e.Message, "\x00")
+
+	return err
+}
