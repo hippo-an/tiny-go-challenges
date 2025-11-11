a67c212b417b70bc421f000e8eb5afabfd15b379
hippo-an <dkstpgud@gmail.com>
2025-11-11 22:10:10 +0900

    http clilent post request

---
+++ netpro_99/http_basic/post_test.go
@@ -0,0 +1,153 @@
+package httpbasic
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"mime/multipart"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"path/filepath"
+	"testing"
+	"time"
+)
+
+type User struct {
+	First string
+	Last  string
+}
+
+// post handler 함수
+func handlePostUser(t *testing.T) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		defer func(r io.ReadCloser) {
+
+			// 서버에서는 명시적으로 요청 본문을 닫기전에 소비해야함
+			_, _ = io.Copy(io.Discard, r)
+			_ = r.Close()
+		}(r.Body)
+
+		if r.Method != http.MethodPost {
+			http.Error(w, "", http.StatusMethodNotAllowed)
+			return
+		}
+
+		var u User
+		err := json.NewDecoder(r.Body).Decode(&u)
+		if err != nil {
+			t.Error(err)
+			http.Error(w, "Decode Failed", http.StatusBadRequest)
+			return
+		}
+		w.WriteHeader(http.StatusAccepted)
+	}
+}
+
+func TestPostUser(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(handlePostUser(t)))
+	defer ts.Close()
+
+	resp, err := http.Get(ts.URL)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// 잘못된 타입의 요청
+	if resp.StatusCode != http.StatusMethodNotAllowed {
+		t.Fatalf("expected status %d; actual status %d", http.StatusMethodNotAllowed, resp.StatusCode)
+	}
+
+	buf := new(bytes.Buffer)
+	u := User{First: "Sehyeong", Last: "An"}
+
+	err = json.NewEncoder(buf).Encode(&u)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Content-Type 의 헤더값을 application/json 으로 설정
+	resp, err = http.Post(ts.URL, "application/json", buf)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if resp.StatusCode != http.StatusAccepted {
+		t.Fatalf("expected status %d; actual status %d", http.StatusAccepted, resp.StatusCode)
+	}
+	_ = resp.Body.Close()
+
+}
+
+func TestMultiPartPost(t *testing.T) {
+	reqBody := new(bytes.Buffer)
+	w := multipart.NewWriter(reqBody)
+
+	for k, v := range map[string]string{
+		"date":        time.Now().Format(time.RFC3339),
+		"description": "Form values with attached files",
+	} {
+		err := w.WriteField(k, v)
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	for i, file := range []string{
+		"./files/hello.txt",
+		"./files/goodbye.txt",
+	} {
+		filePart, err := w.CreateFormFile(fmt.Sprintf("file%d", i+1), filepath.Base(file))
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		f, err := os.Open(file)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		_, err = io.Copy(filePart, f)
+		_ = f.Close()
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	err := w.Close()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 60*time.Second)
+	defer cancel()
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodPost, "https://httpbin.org/post", reqBody)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	req.Header.Set("Content-Type", w.FormDataContentType())
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer func() {
+		_ = resp.Body.Close()
+	}()
+
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		t.Fatalf("expected status %d; actual status %d", http.StatusOK, resp.StatusCode)
+	}
+	t.Logf("\n%s", b)
+
+}
